8fc1990db2b8a61f70b1165becf6bb9677fceccd
coder111 <coder111@protonmail.com>
2022-01-30 15:50:26 +0000

    Change version to 1.1-SNAPSHOT. It should be -SNAPSHOT while the project is in rapid development. Add some methods for PerspectiveCamera.

--- src/main/java/eu/bebendorf/threejava/camera/PerspectiveCamera.java
+++ src/main/java/eu/bebendorf/threejava/camera/PerspectiveCamera.java
@@ -1,7 +1,21 @@
 package eu.bebendorf.threejava.camera;
 
+import org.teavm.jso.JSProperty;
+
 public interface PerspectiveCamera extends Camera {
 
+    /**
+     * Camera frustum aspect ratio, usually the canvas width / canvas height. Default is 1 (square canvas).
+     * @return 
+     */
+    @JSProperty
+    float getAspect();
+    @JSProperty
+    void setAspect(float aspect);
 
+    /**
+     * Updates the camera projection matrix. Must be called after any change of parameters. 
+     */
+    void updateProjectionMatrix();
 
 }
